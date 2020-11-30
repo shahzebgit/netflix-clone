34441cacffaee1fcef1e6514220c64ffbbf5c085
shahzebgit <shahzebparkar@gmail.com>
2020-11-30 12:58:10 +0530

    Added: Created a Form page for Sign in

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Jumbotron } from "./jumbotron/jumbotron-component";
 export { default as Footer } from "./footer/footer-component";
 export { default as Accordion } from "./accordion/accordion-component";
-export {default as OptForm } from './opt-form/opt-form-component'
-export {default as Header} from './header/header-component.jsx'
-export{default as Feature} from './feature/feature-component'
+export { default as OptForm } from "./opt-form/opt-form-component";
+export { default as Header } from "./header/header-component.jsx";
+export { default as Feature } from "./feature/feature-component";
+export { default as Form } from "./form/form-component";
