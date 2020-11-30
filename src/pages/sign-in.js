54dbb4ce182660349fdfecbf4ed4f456143e86d7
shahzebgit <shahzebparkar@gmail.com>
2020-11-30 13:00:20 +0530

    Update: Created Sign in page with Authentication and validation for Form

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,7 +1,79 @@
+import { useContext, useState } from "react";
+import { Form } from "../components";
+import { FirebaseContext } from "../context/firebase";
+import { FooterContainer } from "../containers/footer-container";
+import { HeaderContainer } from "../containers/header-container";
+import * as ROUTES from "../constants/routes";
+import { useHistory } from "react-router-dom";
 
+export default function SignIn() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
-export default function SignIn(){
-    return(
-        <p>Hello Sign in</p>
-    )
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  //validation for disable button
+  const isInValid = password === "" || emailAddress === "";
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    //firebase auth
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
+
+  return (
+    <>
+      {/* Check for Input element */}
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error data-testid="error">{error}</Form.Error>}
+
+          {/* //Email && password validation */}
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInValid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.TextSmall type="checkbox">Remember me</Form.TextSmall>
+          {/* <Form.TextSmall type="checkbox">Remember me</Form.TextSmall> */}
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign Up now.</Form.Link>
+          </Form.Text>
+
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <Form.Link to="/">Learn more.</Form.Link>
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
