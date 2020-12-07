3d58d99ad0c4f3d830b959d87d318e2e32423b44
shahzebgit <shahzebparkar@gmail.com>
2020-12-07 21:28:09 +0530

    Update: Styled the components

--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -17,7 +17,7 @@ import {
   Text,
   Feature,
   Logo,
-} from "./styles/header-styles";
+} from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
